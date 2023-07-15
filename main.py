fa32ab1feab63d263c0ecf1f5419da23c6a24ea6
Zahra Oveisi <z.oveisi@ebcom.ir>
2023-07-15 15:42:00 +0330

    define methods of ticket class

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Hotel:
 
     def __init__(self,hotel_id):
         self.hotel_id = hotel_id
+        self.hotel_name = df.loc[df['id']==self.hotel_id,"name"].squeeze()
 
     def book(self):
         """book a hotel and change availability to no"""
@@ -26,11 +27,17 @@ class Hotel:
 class Ticket():
 
     def __init__(self,customer_name,hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel_object = hotel_object
     def reserve(self,customer_name,hotel_object):
         pass
     def generate(self):
-        pass
+        content = f"""
+                 Thank you for your reservation.
+                 your information is:
+                 name : {self.customer_name}
+                 hotel : {self.hotel_object.hotel_name}"""
+        return content
 
 hotel_ID = input("Enter the ID of hotel: ")
 hotel = Hotel(hotel_ID)
